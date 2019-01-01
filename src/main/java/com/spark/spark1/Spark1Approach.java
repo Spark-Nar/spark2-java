329880c0b4915ca1c6f01bef904a057bd1ab1ed5
Narendra Nirala <nknirala.007@gmail.com>
2019-01-01 23:59:36 +0530

    Spark 1 RDD changes

--- src/main/java/com/spark/spark1/Spark1Approach.java
+++ src/main/java/com/spark/spark1/Spark1Approach.java
@@ -1,8 +1,11 @@
 package com.spark.spark1;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +18,25 @@ public class Spark1Approach {
         //Parallelized Collections
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data);
+
+        //External Dataset / RDD basics
+        JavaRDD<String> distFile = sc.textFile("F:\\Tutorials\\BigData\\Spark\\TextFileForSpark\\sparkReadTest.txt");
+        JavaRDD<Integer> lineLengths = distFile.map(s -> s.length());
+        //If we also wanted to use lineLengths again later, we could add:
+        lineLengths.persist(StorageLevel.MEMORY_ONLY());
+
+        int size = lineLengths.reduce((a, b) -> a + b);
+        System.out.println("File size is "+size);
+
+        //Other methods for directory
+        //textFile("/my/directory"), textFile("/my/directory/*.txt"), and textFile("/my/directory/*.gz").
+
+        //Working with key value pair
+        JavaRDD<String> lines = sc.textFile("F:\\Tutorials\\BigData\\Spark\\TextFileForSpark\\sparkReadTest.txt");
+        JavaPairRDD<String, Integer> pairs = lines.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairRDD<String, Integer> counts = pairs.reduceByKey((a, b) -> a + b);
+
+        sc.close();
 
     }
 }
